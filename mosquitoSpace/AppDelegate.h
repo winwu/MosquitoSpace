1acdab3b987a94a01b06d3bbbc1684e827cd5324
Win Wu <yiyingwu.1990@gmail.com>
2014-03-27 21:05:25 +0800

    INIT  new project

---
+++ mosquitoSpace/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  mosquitoSpace
+//
+//  Created by win on 2014/3/27.
+//  Copyright (c) 2014年 win. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ mosquitoSpace/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  mosquitoSpace
+//
+//  Created by win on 2014/3/27.
+//  Copyright (c) 2014年 win. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
