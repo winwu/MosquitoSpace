eed8325acc07d67a0aa7e3733adc2b442b633c52
Win Wu <yiyingwu.1990@gmail.com>
2014-03-27 21:19:33 +0800

    create RootViewController

---
+++ RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  mosquitoSpace
+//
+//  Created by win on 2014/3/27.
+//  Copyright (c) 2014年 win. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITabBarController
+
+@end
