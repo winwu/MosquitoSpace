7a78c5410e761e76e4e874f09279ecbd50d762a9
Win Wu <yiyingwu.1990@gmail.com>
2014-03-27 21:41:22 +0800

    init DetailViewController

---
+++ DetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.h
+//  mosquitoSpace
+//
+//  Created by win on 2014/3/27.
+//  Copyright (c) 2014年 win. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@end
