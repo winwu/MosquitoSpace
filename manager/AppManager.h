b8be4353ed8b9072827602d8e95169a97224530d
Win Wu <yiyingwu.1990@gmail.com>
2014-04-30 22:45:25 +0800

    建立 appManager

---
+++ manager/AppManager.h
@@ -0,0 +1,13 @@
+//
+//  AppManager.h
+//  mosquitoSpace
+//
+//  Created by win on 2014/4/30.
+//  Copyright (c) 2014年 win. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AppManager : NSObject
+
+@end
---
+++ manager/AppManager.m
@@ -0,0 +1,13 @@
+//
+//  AppManager.m
+//  mosquitoSpace
+//
+//  Created by win on 2014/4/30.
+//  Copyright (c) 2014年 win. All rights reserved.
+//
+
+#import "AppManager.h"
+
+@implementation AppManager
+
+@end
